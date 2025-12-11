802dd216729edcd92bdd220d133c17829eefe23e
최승언 <chygold77@naver.com>
2025-12-11 10:50:17 +0900

    [Feat] 좋아요 Repository 구현

---
+++ src/main/java/com/ureka/techpost/domain/likes/repository/LikesRepository.java
@@ -0,0 +1,14 @@
+package com.ureka.techpost.domain.likes.repository;
+
+
+import com.ureka.techpost.domain.likes.entity.Likes;
+import com.ureka.techpost.domain.post.entity.Post;
+import com.ureka.techpost.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface LikesRepository extends JpaRepository<Likes, Long> {
+    boolean existsByUserAndPost(User user, Post post);
+    Optional<Likes> findByUserAndPost(User user, Post post);
+}
